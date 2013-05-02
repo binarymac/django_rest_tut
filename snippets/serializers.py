f27795d2846f49d62debe983425e3863914af46b
Michael Ackerman <binarymac@gmail.com>
2013-05-01 21:18:56 -0700

    finished part 4 of tutorial, removed migrations

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,9 +1,27 @@
-from django.forms import widgets
 from rest_framework import serializers
-from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from snippets.models import Snippet
+from django.contrib.auth.models import User
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.Field(source='owner.username')
+
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'lineos', 'language', 'style',)
+        fields = (
+            'id',
+            'title',
+            'code',
+            'linenos',
+            'language',
+            'style',
+            'owner',
+        )
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets',)
--- snippets/views.py
+++ snippets/views.py
@@ -1,13 +1,37 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import permissions
 
 
 class SnippetList(generics.ListCreateAPIView):
     model = Snippet
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def pre_save(self, obj):
+        obj.owner = self.request.user
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     model = Snippet
     serializer_class = SnippetSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+    )
+
+    def pre_save(self, obj):
+        obj.owner = self.request.user
+
+
+class UserList(generics.ListAPIView):
+    model = User
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    model = User
+    serializer_class = UserSerializer
